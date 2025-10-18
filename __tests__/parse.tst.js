9e2287cbed8d610ff9d25633b924cbb45f64bc52
Tim Tessier <flieingzbu@gmail.com>
2025-10-17 20:42:02 -0400

    *Remove superfluous __tests__ folder in favor of test.js

--- __tests__/parse.tst.js
+++
@@ -1,5 +0,0 @@
-const parser = require('../cparser');
-
-test('test basic parse', () => {
-  expect(parser.parse("void main(){}","main.c")).toStrictEqual([{"arguments": [], "body": [], "defType": {"modifier": [], "name": "void", "pos": 0, "type": "Type"}, "name": "main", "pos": 0, "type": "FunctionDefinition"}]);
-});
